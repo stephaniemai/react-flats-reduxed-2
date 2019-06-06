0be59347bb848ad23cdecab6c0bcd599ae8dc44c
stephaniemai <stephaniemai.jacques@gmail.com>
2019-06-06 09:38:08 +0200

    add redux-logger and api call middleware with redux-promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
-import flats from '../../data/flats';
+// import flats from '../../data/flats';
 
 export function setFlats() {
-  return {
-    type: 'SET_FLATS',
-    payload: flats
-  };
+  // return {
+  //   type: 'SET_FLATS',
+  //   payload: flats
+  // };
+
+  return fetch('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json')
+    .then(response => response.json())
+    .then((data) => {
+      return {
+        type: 'SET_FLATS',
+        payload: data
+      };
+    });
 }
 
 export function selectFlat(flat) {
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
+// Internal modules
 import '../assets/stylesheets/application.scss';
-import flatReducer from './reducers/flats_reducer';
-import selectedFlatReducer from './reducers/selected_flat_reducer';
-
 import App from './components/app';
 
-// const root = document.getElementById('root');
+// Reducers
+import flatReducer from './reducers/flats_reducer';
+import selectedFlatReducer from './reducers/selected_flat_reducer';
 
 const reducers = combineReducers({
   flats: flatReducer,
   selectedFlat: selectedFlatReducer
 });
 
+const middlewares = applyMiddleware(reduxPromise, logger);
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
